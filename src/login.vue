ae1e3e06815d6ba1831010b5fdf4b3a74a451475
algfwq <3104374883@qq.com>
2023-05-27 15:41:17 +0800

    修改

--- src/login.vue
+++ src/login.vue
@@ -1,5 +1,14 @@
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue';
+import { ref, onMounted, onUnmounted,reactive } from 'vue';
+
+//设置深色模式
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+
+//设置骨架屏
+const loading = ref(true)
 
 // 获取当前页面的主机名和端口号
 const hostname = window.location.hostname;
@@ -27,16 +36,34 @@ socket.onmessage = function(message){
 
   //如果为加载状态
   if(data.mode === "load"){
+    //关闭骨架屏
+    loading.value = false
+
     //更改网页标题
     const title = ref(data.web_name);
-    document.title = title.value;
+    document.title = '登录 - ' + title.value;
+    let ltitle = ref('登录 - ' + title.value)
   }
 }
 </script>
 
 <template>
-  <el-button>奥利给！</el-button>
+  <!-- 内容骨架屏 -->
+  <el-skeleton style="width: 100%" direction="vertical" alignment="flex-start" :loading="loading" animated class="content-skeleton">
+    <!-- 骨架屏内容 -->
+    <template #template>
+      <div style="text-align: -webkit-center">
+        <br><br><br><br>
+        <el-skeleton-item variant="rect" style="width: 30%;height: 430px"/>
+      </div>
+    </template>
+    <!-- 正文 -->
+    <template #default>
+      <el-button>奥利给！</el-button>
+    </template>
+  </el-skeleton>
 </template>
 
 <style scoped>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import login from './login.vue'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
-createApp(login).use(ElementPlus).mount('#login')
+createApp(login).use(ElementPlus,{dark:'auto'}).mount('#login')
