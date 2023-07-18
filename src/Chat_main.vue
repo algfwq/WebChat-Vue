38b4bd248af1f301ed693554accb4658b6f5805e
algfwq <3104374883@qq.com>
2023-07-18 21:28:42 +0800

    骨架屏

---
+++ src/Chat_main.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref, onMounted, onUnmounted,reactive } from 'vue'
+import Cookies from 'js-cookie';
+
+//设置深色模式
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+
+//设置骨架屏
+const loading = ref(true)
+
+//骨架屏高度自适应
+function autoheight1(id){
+  function adjustheight(id) {
+    // 获取指定div元素对象
+    var divElement = document.getElementById(id);
+    divElement.style.height = (window.innerHeight - 20).toString() + "px";
+  }
+  window.setInterval(function logname() {adjustheight(id)}, 1);
+}
+autoheight1("sk_aside");
+function autoheight2(id){
+  function adjustheight(id) {
+    // 获取指定div元素对象
+    var divElement = document.getElementById(id);
+    divElement.style.height = (window.innerHeight - 40).toString() + "px";
+  }
+  window.setInterval(function logname() {adjustheight(id)}, 1);
+}
+autoheight2('sk_main');
+</script>
+
+<template>
+  <!-- 内容骨架屏 -->
+  <el-skeleton style="width: 100%" direction="vertical" alignment="flex-start" :loading="loading" animated class="content-skeleton">
+    <!-- 骨架屏内容 -->
+    <template #template>
+      <el-container width="100%">
+        <el-aside width="50px"><el-skeleton-item variant="rect" style="width: 100%" id="sk_aside"/></el-aside>
+        <el-main><el-skeleton-item variant="rect" style="width: 100%" id="sk_main"/></el-main>
+      </el-container>
+    </template>
+    <!-- 正文 -->
+    <template #default>
+    </template>
+  </el-skeleton>
+</template>
+
+<style scoped>
+.el-container {
+  height: 100%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import login from './login.vue'
+import Chat_main from './Chat_main.vue'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 
-createApp(login).use(ElementPlus,{dark:'auto'}).mount('#login')
+createApp(Chat_main).use(ElementPlus,{dark:'auto'}).mount('#Chat_main')
